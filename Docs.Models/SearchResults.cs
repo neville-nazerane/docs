e769e854883b41a8495a21154e743e839bdf4be9
Neville <live.neville@live.com>
2018-07-30 12:22:59 -0400

    search created

---
+++ Docs.Models/SearchResults.cs
@@ -0,0 +1,16 @@
+﻿using Docs.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Docs.Models
+{
+    public class SearchResults
+    {
+
+        public IEnumerable<Package> Packages { get; set; }
+
+        public Dictionary<string, IEnumerable<Package>> GitRepos { get; set; }
+
+    }
+}
---
+++ Docs.Website/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Docs.Data;
+using Docs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Website.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public SearchController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index(string q)
+        {
+            ViewBag.q = q;
+
+            return View(new SearchResults
+            {
+                Packages = from pt in context.PackageTags
+                           where pt.Package.Name.Contains(q, StringComparison.InvariantCultureIgnoreCase) 
+                                            || q.Contains(pt.Tag.Title, StringComparison.InvariantCultureIgnoreCase)
+                           select pt.Package,
+                GitRepos = context.Packages.Where(p => p.GitRepo.Contains(q))
+                                             .GroupBy(p => p.GitRepo)
+                                             .ToDictionary(g => g.Key, g => g.AsEnumerable())
+            });
+        }
+    }
+}
