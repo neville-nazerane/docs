cb73584936ddc9cd6df4a9eaf8e29b8f8a244b5e
Neville <live.neville@live.com>
2018-08-31 11:07:31 -0400

    client ui mappers

--- Docs.Website/Controllers/NetCore_Apis_Client_UIController.cs
+++ Docs.Website/Controllers/NetCore_Apis_Client_UIController.cs
@@ -1,4 +1,6 @@
 ﻿using Docs.Website.Filters;
+using Docs.Website.Models;
+using Docs.Website.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,11 +10,24 @@ using System.Threading.Tasks;
 namespace Docs.Website.Controllers
 {
 
-    [Documentation("NetCore.Apis.Client.UI")]
+    [Documentation("NetCore.Apis.Client.UI", nameof(Intro))]
     public class NetCore_Apis_Client_UIController : Controller
     {
 
-        public IActionResult Index() => View();
+        public NetCore_Apis_Client_UIController(CurrentDocument document)
+        {
+            document.TabItems = new TabItem[] {
+                new TabItem(nameof(Intro)),
+                new TabItem(nameof(Mapping)),
+                new TabItem(nameof(Submit)),
+            };
+        }
+
+        public IActionResult Intro() => View();
+
+        public IActionResult Mapping() => View();
+
+        public IActionResult Submit() => View();
 
     }
 }
