239238702a48a43149a899348b1834a3759dafa9
Neville <live.neville@live.com>
2018-11-29 22:20:16 -0500

    updated libraries

--- Docs.Website/Program.cs
+++ Docs.Website/Program.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Docs.Website
 {
@@ -19,12 +22,32 @@ namespace Docs.Website
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(
-                    new ConfigurationBuilder()
-                    .AddJsonFile(
-                            Path.Combine(Directory.GetCurrentDirectory(), @"..\secrets\nev_docs.json"), true)
-                    .Build()
-                )
+                .UseConfiguration(ExternalConfiguration())
                 .UseStartup<Startup>();
+
+        static IConfiguration ExternalConfiguration()
+            => KeyVaultConfiguration() ?? FileConfiguration();
+
+        static IConfiguration FileConfiguration()
+            => new ConfigurationBuilder()
+                    .AddJsonFile(
+                            Path.Combine(Directory.GetCurrentDirectory(), 
+                                            @"..\secrets\nev_docs.json"), true)
+                    .Build();
+
+        static IConfiguration KeyVaultConfiguration()
+        {
+            var vaultEndPoint = Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+            if (vaultEndPoint == null) return null;
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+            return new ConfigurationBuilder()
+                    .AddAzureKeyVault(vaultEndPoint, keyVaultClient, 
+                                        new DefaultKeyVaultSecretManager())
+                    .Build();
+        }
+
     }
 }
