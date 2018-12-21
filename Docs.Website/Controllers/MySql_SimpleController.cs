474809781c2d551764beff2a30ce4def53ed2684
Neville <live.neville@live.com>
2018-12-20 19:26:00 -0500

    mysql simple added

---
+++ Docs.Website/Controllers/MySql_SimpleController.cs
@@ -0,0 +1,33 @@
+﻿using Docs.Website.Filters;
+using Docs.Website.Models;
+using Docs.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Website.Controllers
+{
+
+    [Documentation("MySql.Simple", nameof(Connecting))]
+    public class MySql_SimpleController : Controller
+    {
+
+        public MySql_SimpleController(CurrentDocument document)
+        {
+            document.TabItems = new TabItem[] {
+                new TabItem(nameof(Connecting)),
+                new TabItem(nameof(Quering)),
+                new TabItem(nameof(CodeFriendly)),
+            };
+        }
+
+        public IActionResult Connecting() => View();
+
+        public IActionResult Quering() => View();
+
+        public IActionResult CodeFriendly() => View();
+
+    }
+}
---
+++ Docs.Website/Controllers/NetCore_JwtController.cs
@@ -0,0 +1,30 @@
+﻿using Docs.Website.Filters;
+using Docs.Website.Models;
+using Docs.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Website.Controllers
+{
+
+    [Documentation("NetCore.Jwt", nameof(SetUp))]
+    public class NetCore_JwtController : Controller
+    {
+
+        public NetCore_JwtController(CurrentDocument current)
+        {
+            current.TabItems = new TabItem[] {
+                new TabItem(nameof(SetUp)),
+                new TabItem(nameof(FetchingTokens)),
+            };
+        }
+
+        public IActionResult SetUp() => View();
+
+        public IActionResult FetchingTokens() => View();
+
+    }
+}
