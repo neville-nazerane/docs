a03b89f40939fb13a4575ec46cf946700f1c0586
Neville <live.neville@live.com>
2019-02-12 21:05:46 -0500

    updated consume api

--- Docs.Website/Controllers/NetCore_Apis_ConsumerController.cs
+++ Docs.Website/Controllers/NetCore_Apis_ConsumerController.cs
@@ -1,4 +1,6 @@
 ﻿using Docs.Website.Filters;
+using Docs.Website.Models;
+using Docs.Website.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,11 +10,24 @@ using System.Threading.Tasks;
 namespace Docs.Website.Controllers
 {
 
-    [Documentation("NetCore.Apis.Consumer")]
+    [Documentation("NetCore.Apis.Consumer", nameof(Consumer))]
     public class NetCore_Apis_ConsumerController : Controller
     {
 
-        public IActionResult Index() => View();
+        public NetCore_Apis_ConsumerController(CurrentDocument document)
+        {
+            document.TabItems = new TabItem[] {
+                new TabItem(nameof(Consumer)),
+                new TabItem(nameof(MakingCalls)),
+                new TabItem(nameof(ConsumedResponse)),
+            };
+        }
+
+        public IActionResult Consumer() => View();
+
+        public IActionResult MakingCalls() => View();
+
+        public IActionResult ConsumedResponse() => View();
 
     }
 }
