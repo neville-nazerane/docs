500496ad24b4ac29aeafeaf518c96e7c35a21b2c
Neville <live.neville@live.com>
2019-08-12 00:48:14 -0700

    path prefix

---
+++ Docs.Website/TagHelpers/JsonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Website.TagHelpers
+{
+    public class JsonTagHelper : TagHelper
+    {
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+            var c = await output.GetChildContentAsync();
+            string content = c.GetContent().Trim();
+            output.TagName = "pre";
+            output.Content.SetHtmlContent($"<code class='csharp'>{content}</code>");
+        }
+
+    } 
+}
