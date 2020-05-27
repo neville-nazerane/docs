5654067611a7c0e0757b0ba2c38cb55fd0cc0957
Neville <live.neville@live.com>
2020-05-27 01:23:43 -0700

    added a doc (kindda)

---
+++ src/Docs.Website/Client/Components/CodeBlock.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Website.Client.Components
+{
+    public partial class CodeBlock
+    {
+
+        [Parameter]
+        public string Language { get; set; }
+
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ src/Docs.Website/Client/Documenations/NetCore_Azure_Blob.razor.cs
@@ -0,0 +1,60 @@
+﻿using Docs.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Docs.Website.Client.Documenations
+{
+    public partial class NetCore_Azure_Blob
+    {
+
+        CurrentDocument Document => new CurrentDocument
+        {
+            Package = new Package { }
+        };
+
+        class CurrentDocument
+        {
+            public Package Package { get; set; }
+
+            public IEnumerable<TabItem> TabItems { get; set; }
+
+            public string CurrentTab { get; set; }
+
+            public bool HasTabs => TabItems?.Count() > 0;
+
+        }
+
+
+        public class TabItem
+        {
+
+            public TabItem()
+            {
+
+            }
+
+            public TabItem(string all)
+            {
+                All = all;
+            }
+
+            public string Title { get; set; }
+
+            public string Action { get; set; }
+
+            public string All
+            {
+                set
+                {
+                    Action = value;
+                    Title = Regex.Replace(value, "([a-z])([A-Z])", "$1 $2");
+                }
+            }
+
+        }
+
+    }
+}
