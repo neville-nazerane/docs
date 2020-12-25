ccc44bae6c6095c7d76a88a2de9255d89088edd3
Neville <live.neville@live.com>
2020-12-25 04:47:09 -0700

    a new hope

---
+++ src/Docs.WebApp/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Docs.WebApp.Models
+{
+    public class Tag
+    {
+
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Title { get; set; }
+
+        public int? Importance { get; set; }
+
+    }
+}
---
+++ src/Docs.WebApp/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Docs.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.WebApp.Pages
+{
+    public partial class Index
+    {
+
+        IEnumerable<Package> packages = null;
+
+
+    }
+}
