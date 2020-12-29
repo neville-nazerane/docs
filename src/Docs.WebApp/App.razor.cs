9a782ca035723ddbe9dc6e370a9a3ab0cf9a234d
Neville <live.neville@live.com>
2020-12-29 04:13:39 -0700

    documentation layout logic

---
+++ src/Docs.WebApp/App.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.WebApp
+{
+    public partial class App
+    {
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        bool IsDocumentation
+        {
+            get
+            {
+
+                var path = NavigationManager.Uri?.Replace(NavigationManager.BaseUri, string.Empty);
+                Console.WriteLine(path);
+                if (string.IsNullOrEmpty(path))
+                    return false;
+                return path
+                        .Split("/")
+                        .FirstOrDefault()?
+                        .Equals("documentation", StringComparison.OrdinalIgnoreCase)
+                                == true;
+            }
+        }
+    }
+}
