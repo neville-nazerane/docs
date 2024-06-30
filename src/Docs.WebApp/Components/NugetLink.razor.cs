49b64aa2def770f1dda106b007e3cccfe13baf07
Neville Nazerane <live.neville@live.com>
2024-06-29 23:58:15 -0700

    introduction setup

---
+++ src/Docs.WebApp/Components/NugetLink.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Docs.WebApp.Components
+{
+    public partial class NugetLink
+    {
+
+        [Parameter]
+        public string Name { get; set; }
+
+    }
+}
